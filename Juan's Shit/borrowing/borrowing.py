9c4d7c5e146df1a6fe57704bb2cbf22759feff79
Ioannis (Juan) Tsantilas <134544768+JuanTsa@users.noreply.github.com>
2023-05-29 15:10:37 +0300

    Update borrowing.py

--- Juan's Shit/borrowing/borrowing.py
+++ Juan's Shit/borrowing/borrowing.py
@@ -1,7 +1,7 @@
 import random
 import datetime
 
-def generate_random_number(min_val, max_val, used_numbers):
+def generate_unique_random_number(min_val, max_val, used_numbers):
     num = random.randint(min_val, max_val)
     while num in used_numbers:
         num = random.randint(min_val, max_val)
@@ -18,8 +18,8 @@ used_j = set()
 used_id = set()
 
 for i in range(1, 61):
-    j = generate_random_number(1, 150, used_j)
-    id = generate_random_number(1, 80, used_id)
+    j = generate_unique_random_number(1, 150, used_j)
+    id = generate_unique_random_number(1, 80, used_id)
     borrow_date = generate_random_date(start_date, end_date)
     due_date = (datetime.datetime.strptime(borrow_date, "%Y-%m-%d") + datetime.timedelta(days=7)).strftime("%Y-%m-%d")
     returning_date = generate_random_date(datetime.datetime.strptime(borrow_date, "%Y-%m-%d").date(), end_date)
