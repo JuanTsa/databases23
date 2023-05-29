3862f96f8e6b778dd90ebe770dbc20deb9d440b3
Ioannis (Juan) Tsantilas <134544768+JuanTsa@users.noreply.github.com>
2023-05-29 15:02:10 +0300

    Update borrowing.py

--- Juan's Shit/borrowing/borrowing.py
+++ Juan's Shit/borrowing/borrowing.py
@@ -1,8 +1,12 @@
 import random
 import datetime
 
-def generate_random_number(min_val, max_val):
-    return random.randint(min_val, max_val)
+def generate_random_number(min_val, max_val, used_numbers):
+    num = random.randint(min_val, max_val)
+    while num in used_numbers:
+        num = random.randint(min_val, max_val)
+    used_numbers.add(num)
+    return num
 
 def generate_random_date(start_date, end_date):
     random_date = start_date + datetime.timedelta(days=random.randint(0, (end_date - start_date).days))
@@ -10,10 +14,12 @@ def generate_random_date(start_date, end_date):
 
 start_date = datetime.date(2022, 1, 1)
 end_date = datetime.date(2022, 12, 31)
+used_j = set()
+used_id = set()
 
 for i in range(1, 61):
-    j = generate_random_number(1, 150)
-    id = generate_random_number(1, 80)
+    j = generate_random_number(1, 150, used_j)
+    id = generate_random_number(1, 80, used_id)
     borrow_date = generate_random_date(start_date, end_date)
     due_date = (datetime.datetime.strptime(borrow_date, "%Y-%m-%d") + datetime.timedelta(days=7)).strftime("%Y-%m-%d")
     returning_date = generate_random_date(datetime.datetime.strptime(borrow_date, "%Y-%m-%d").date(), end_date)
