d6b27e82a3c10bab290f65b8a61c5178e7414b70
Ioannis (Juan) Tsantilas <134544768+JuanTsa@users.noreply.github.com>
2023-05-31 16:06:23 +0300

    Update book_category.c

--- Dummy_Data/book_category.c
+++ Dummy_Data/book_category.c
@@ -2,11 +2,19 @@
 
 int main() {
    int j=1;
+   //one category for each book
    for(int i=1; i<151; i++){
        printf("(%d, %d),\n", j, i);
        j++;
        if(j==11) j=1;
    }
-   
+   //some books should belong to 2 categories
+   j=2;
+     for(int i=1; i<151; i+=13){
+       printf("(%d, %d),\n", j, i);
+       printf("(%d, %d),\n", j+1, i+1);
+       j++;
+       if(j==11) j=1;
+   }
     return 0;
 }
