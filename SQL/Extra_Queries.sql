802b6bc60c02edf7e1b8b0c74f6a3054e6310492
Ioannis (Juan) Tsantilas <134544768+JuanTsa@users.noreply.github.com>
2023-06-02 00:47:15 +0300

    Create Extra_Queries.sql

---
+++ SQL/Extra_Queries.sql
@@ -0,0 +1,7 @@
+-- I put here some extra queries for the website (mostly are supposed to be only for the operator).
+-- You'd change only the <...> with the desired variable. If something is in this format: '<...>' DO NOT DELETE the single quotes ''.
+
+INSERT INTO `Category` (`Category_Name) VALUES ('<category_name>');
+
+INSERT INTO `Author` (`Author_Name`, `Author_Surname`) VALUES ('<author_name>', '<author_surname>');
+
