294dd28314f3e26f1285d5863e54798c642d80b9
Ioannis (Juan) Tsantilas <134544768+JuanTsa@users.noreply.github.com>
2023-06-01 23:48:56 +0300

    Update Create_Tables.sql

--- SQL/Create_Tables.sql
+++ SQL/Create_Tables.sql
@@ -17,7 +17,7 @@ CREATE TABLE IF NOT EXISTS `User` (
   `Name` varchar(50) NOT NULL,
   `Surname` varchar(50) NOT NULL,
   `Email` varchar(50) NOT NULL,
-  `Birth_Date` date NOT NULL,
+  `Age` int(3) NOT NULL,
   `Copies_Borrowed` int(50) NOT NULL,
   `Copies_Reserved` int(50) NOT NULL,
   `Max_Copies_Borrowed` int(50) NOT NULL,
