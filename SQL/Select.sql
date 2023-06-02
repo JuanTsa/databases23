cc1bdeda4f2663867653cd66b72f08c30852c837
Ioannis (Juan) Tsantilas <134544768+JuanTsa@users.noreply.github.com>
2023-06-02 23:20:06 +0300

    Update Select.sql

--- SQL/Select.sql
+++ SQL/Select.sql
@@ -1,3 +1,4 @@
+---- View Contents
 select * from user;
 select * from school_unit;
 select * from category;
@@ -6,6 +7,10 @@ select * from book;
 select * from book_author;
 select * from book_category;
 select * from reservation;
-
 select * from borrowing;
 select * from review;
+
+---- View triggers
+SELECT TRIGGER_SCHEMA, TRIGGER_NAME, EVENT_OBJECT_TABLE, ACTION_TIMING, ACTION_STATEMENT
+FROM information_schema.TRIGGERS
+WHERE TRIGGER_SCHEMA = 'asd';
