132a6d5b2a242e6380d0dcb1ff7ec8b514a7717e
Ioannis (Juan) Tsantilas <134544768+JuanTsa@users.noreply.github.com>
2023-06-02 22:50:33 +0300

    Update Triggers.sql

--- SQL/Triggers.sql
+++ SQL/Triggers.sql
@@ -1,86 +1,64 @@
-DELIMITER //
-CREATE TRIGGER update_available_copies
-BEFORE INSERT ON Reservation
+---- View triggers
+SELECT TRIGGER_SCHEMA, TRIGGER_NAME, EVENT_OBJECT_TABLE, ACTION_TIMING, ACTION_STATEMENT
+FROM information_schema.TRIGGERS
+WHERE TRIGGER_SCHEMA = 'asd';
+
+
+---- CHANGE Copies_Borrowed/Reserved WHEN New_Borrowing/Reservation
+DELIMITER $$
+CREATE TRIGGER update_user_borrowing
+AFTER INSERT ON `Borrowing`
 FOR EACH ROW
 BEGIN
-  UPDATE Book
-  SET Available_Copies = Available_Copies - 1
-  WHERE Book_ID = NEW.Book_ID;
-END;
-//
-DELIMITER ;
+    UPDATE `User`
+    SET `Copies_Borrowed` = `Copies_Borrowed` + 1
+    WHERE `User_ID` = NEW.`User_ID`;
+END$$
 
-DELIMITER //
-CREATE TRIGGER update_available_copies_borrowing
-BEFORE INSERT ON Borrowing
+DELIMITER $$
+CREATE TRIGGER update_user_borrowing
+AFTER INSERT ON `Reservation`
 FOR EACH ROW
 BEGIN
-  UPDATE Book
-  SET Available_Copies = Available_Copies - 1
-  WHERE Book_ID = NEW.Book_ID;
-END;
-//
-DELIMITER ;
+    UPDATE `User`
+    SET `Copies_Reserved` = `Copies_Reserved` + 1
+    WHERE `User_ID` = NEW.`User_ID`;
+END$$
 
-DELIMITER //
-CREATE TRIGGER update_available_copies_returning
-BEFORE UPDATE ON Borrowing
+
+---- CHANGE Available_Copies WHEN New_Borrowing
+DELIMITER $$
+CREATE TRIGGER update_book_borrowing
+AFTER INSERT ON `Borrowing`
 FOR EACH ROW
 BEGIN
-  IF NEW.Status = 'Approved' AND OLD.Status = 'Rejected' THEN
-    UPDATE Book
-    SET Available_Copies = Available_Copies + 1
-    WHERE Book_ID = NEW.Book_ID;
-  END IF;
-END;
-//
-DELIMITER ;
+    UPDATE `Book`
+    SET `Available_Copies` = `Available_Copies` - 1
+    WHERE `Book_ID` = NEW.`Book_ID`;
+END$$
 
-DELIMITER //
-CREATE TRIGGER update_copies_borrowed
-BEFORE INSERT ON Borrowing
+
+---- CHANGE Available_Copies WHEN Completed_Borrowing
+DELIMITER $$
+CREATE TRIGGER update_book_returning
+AFTER DELETE ON `Borrowing`
 FOR EACH ROW
 BEGIN
-  UPDATE User
-  SET Copies_Borrowed = Copies_Borrowed + 1
-  WHERE User_ID = NEW.User_ID;
-END;
-//
-DELIMITER ;
+    UPDATE `Book`
+    SET `Available_Copies` = `Available_Copies` + 1
+    WHERE `Book_ID` = OLD.`Book_ID`;
+END$$
 
-DELIMITER //
-CREATE TRIGGER update_copies_borrowed_returning
-BEFORE UPDATE ON Borrowing
+
+---- WHEN Duplicate_Review
+DELIMITER $$
+CREATE TRIGGER prevent_duplicate_reviews
+BEFORE INSERT ON `Review`
 FOR EACH ROW
 BEGIN
-  IF NEW.Status = 'Approved' AND OLD.Status = 'Rejected' THEN
-    UPDATE User
-    SET Copies_Borrowed = Copies_Borrowed + 1
-    WHERE User_ID = NEW.User_ID;
-  END IF;
-  IF NEW.Status = 'Rejected' AND OLD.Status = 'Approved' THEN
-    UPDATE User
-    SET Copies_Borrowed = Copies_Borrowed - 1
-    WHERE User_ID = NEW.User_ID;
-  END IF;
-END;
-//
-DELIMITER ;
-
-DELIMITER //
-CREATE TRIGGER enforce_max_copies_borrowed
-BEFORE INSERT ON Borrowing
-FOR EACH ROW
-BEGIN
-  DECLARE borrowed_count INT;
-  SELECT COUNT(*) INTO borrowed_count
-  FROM Borrowing
-  WHERE User_ID = NEW.User_ID AND (Status = 'Approved' OR Status = 'On Hold');
-
-  IF borrowed_count >= (SELECT Max_Copies_Borrowed FROM User WHERE User_ID = NEW.User_ID) THEN
-    SIGNAL SQLSTATE '45000'
-    SET MESSAGE_TEXT = 'User has reached the maximum number of borrowed copies.';
-  END IF;
-END;
-//
-DELIMITER ;
+    DECLARE review_count INT;
+    SET review_count = (SELECT COUNT(*) FROM `Review` WHERE `Book_ID` = NEW.`Book_ID` AND `User_ID` = NEW.`User_ID`);
+    IF review_count > 0 THEN
+        SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Duplicate review not allowed.';
+    END IF;
+END$$
