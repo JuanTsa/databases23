a9407ce613b2e6180542cf176dd52304891725d8
Ioannis (Juan) Tsantilas <134544768+JuanTsa@users.noreply.github.com>
2023-06-02 19:55:05 +0300

    Update Extra_Queries.sql

--- SQL/Extra_Queries.sql
+++ SQL/Extra_Queries.sql
@@ -5,36 +5,54 @@
 -- ----------------
 -- ADMINISTRATOR
 -- ----------------
----- adds a new school unit
+---- New_School
 INSERT INTO `School_Unit` (`School_Name`, `Address`, `Phone`, `Email`, `Principal_Name`, `Principal_Surname`) VALUES ('<school_name>', '<address>', <phone>, '<email>', '<principal_name>', '<principal_surname>');
 
----- alters a school unit
+---- Alter_School
 
----- adds a new operator (so by default some values)
+---- New_Operator (so by default some values)
 INSERT INTO `User` (`School_ID`, `Username`, `Password`, `Name`, `Surname`, `Email`, `Age`, `Copies_Borrowed`, `Copies_Reserved`, `Max_Copies_Borrowed`, `Max_Copies_Reserved`, `User_Type`, `Status`) 
 VALUES ('<school_id>', '<username>', '<password>', '<name>', '<surname>', '<email>', <age>, 0, 0, 0, 0, 'Operator', 'Approved');
 
----- alters a operator
+---- Alter_Operator
 
----- lists with all un-approved operators
+---- Approve_Operator
 
----- backups
+---- Backup
 
----- restores
+---- Restore
 
 
 
 -- ----------------
 -- OPERATOR (only for their school)
 -- ----------------
----- adds a new user
+---- (-) New_User
+---- (-) Alter_User
+---- Approve_User
+---- Delete_User
 
----- alters a user
+---- New_Book
+---- Alter_Book
+---- (-) Delete_Book
 
----- adds a new category 
+---- (-) New_Loan
+---- Approve_Loan
+---- Show_Loans_Per_User (-history and active)
+---- New_Return
+---- Delayed_Loans
+
+---- (-) New_Reservation
+---- Approve_Reservation
+---- Show_Reservations_Per_User (-history and active)
+
+---- Approve_Review
+---- Show_Reviews_Per_User
+
+---- New_Category
 INSERT INTO `Category` (`Category_Name`) VALUES ('<category_name>');
 
----- adds a new author
+---- New_Author
 INSERT INTO `Author` (`Author_Name`, `Author_Surname`) VALUES ('<author_name>', '<author_surname>');
 
 ---- adds a new user
@@ -61,3 +79,15 @@ INSERT INTO `Borrowing` (`Borrowing_ID`, `Book_ID`, `User_ID`, `Borrow_Date`, `D
 INSERT INTO `Reservation` (`Reservation_ID`, `Book_ID`, `User_ID`, `Request_Date`, `Status`) VALUES (1, 62, 60, '2022-07-11', 'Approved'),
 
 INSERT INTO `Review` (`Review_ID`, `Review_Text`, `Book_ID`, `User_ID`, `Rating`, `Status`) VALUES (1, 'An awesome, page-turner, book. My review does not apply to my rating!', 28, 22, 5, 'Approved');
+
+-- ----------------
+-- STUDENTS AND TEACHERS
+-- ----------------
+---- Show_My_Loans
+---- Show_My_Reservatiosn
+---- Show_My_Reviews
+
+---- New_Loan
+---- New_Reservation
+---- New_Review
+---- (-) Cancel_Reservation
