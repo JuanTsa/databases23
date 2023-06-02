fddfacb4abb2222b2a788f79ba65f83396507fe0
Ioannis (Juan) Tsantilas <134544768+JuanTsa@users.noreply.github.com>
2023-06-02 20:06:16 +0300

    Update Extra_Queries.sql

--- SQL/Extra_Queries.sql
+++ SQL/Extra_Queries.sql
@@ -38,12 +38,21 @@ VALUES ('<school_id>', '<username>', '<password>', '<name>', '<surname>', '<emai
 
 ---- (-) New_Loan
 ---- Approve_Loan
----- Show_Loans_Per_User ((-)history and active)
+
+---- Show_Loans_Per_User (history)
 SELECT b.Borrowing_ID, b.Book_ID, bk.Title, u.Username, u.Name, u.Surname, b.Borrow_Date, b.Due_Date, b.Returning_Date
 FROM Borrowing b
 INNER JOIN User u ON b.User_ID = u.User_ID
 INNER JOIN Book bk ON b.Book_ID = bk.Book_ID
 WHERE u.School_ID = (SELECT School_ID FROM User WHERE User_ID = <operator_user_id>);			-- Replace with the desired element
+
+---- Show_Loans_Per_User (active)
+SELECT b.Borrowing_ID, b.Book_ID, bk.Title, u.Username, u.Name, u.Surname, b.Borrow_Date, b.Due_Date, b.Returning_Date
+FROM Borrowing b
+INNER JOIN User u ON b.User_ID = u.User_ID
+INNER JOIN Book bk ON b.Book_ID = bk.Book_ID
+WHERE u.School_ID = (SELECT School_ID FROM User WHERE User_ID = <operator_user_id>)			-- Replace with the desired element
+  AND b.Returning_Date IS NULL;
 
 ---- New_Return
 ---- Delayed_Loans
