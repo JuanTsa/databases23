12b9c840b4e81006783f0a7c950befe512e5a321
Ioannis (Juan) Tsantilas <134544768+JuanTsa@users.noreply.github.com>
2023-06-03 23:34:02 +0300

    Create del_bor.c

---
+++ New_Dummy_Data/del_bor.c
@@ -0,0 +1,32 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    // Write C code here
+    int i=368;
+    for(int j=0; j<5; j++)
+        printf("(%i, 299, , '2023-05-27', 'Approved'),\n", i++);
+
+    for(int j=0; j<5; j++)
+        printf("(%i, 293, , '2023-05-27', 'Approved'),\n", i++);
+    
+    for(int j=0; j<5; j++)
+        printf("(%i, 300, , '2023-05-27', 'Approved'),\n", i++);
+    
+    for(int j=0; j<5; j++)
+        printf("(%i, 287, , '2023-05-27', 'Approved'),\n", i++);
+    
+    for(int j=0; j<5; j++)
+        printf("(%i, 284, , '2023-05-27', 'Approved'),\n", i++);
+
+    for(int j=0; j<5; j++)
+        printf("(%i, 294, , '2023-05-27', 'Approved'),\n", i++);
+    
+    for(int j=0; j<5; j++)
+        printf("(%i, 296, , '2023-05-27', 'Approved'),\n", i++);
+    
+    for(int j=0; j<5; j++)
+        printf("(%i, 298, , '2023-05-27', 'Approved'),\n", i++);
+        
+    return 0;
+}
