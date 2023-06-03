ab8365ec9e72cccf0fc79c6142c351c640dec4d6
Ioannis (Juan) Tsantilas <134544768+JuanTsa@users.noreply.github.com>
2023-06-04 02:29:38 +0300

    Update 4_New_Triggers.sql

--- SQL/4_New_Triggers.sql
+++ SQL/4_New_Triggers.sql
@@ -157,3 +157,28 @@ BEGIN
     SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Available copies exist for this book. You should request a borrowing.';
   END IF;
 END //
+
+-- ----------
+-- TRIGGER 7
+-- ----------
+-- Check whether the user has a delayed borrowing before making a new borrowing
+DELIMITER //
+CREATE TRIGGER check_delayed_borrowing
+BEFORE INSERT ON Borrowing
+FOR EACH ROW
+BEGIN
+  DECLARE delayed_count INT;
+  
+  -- Count the number of delayed borrowings for the user
+  SELECT COUNT(*) INTO delayed_count
+  FROM Borrowing
+  WHERE User_ID = NEW.User_ID
+    AND Returning_Date < CURDATE()
+    AND Status = 'Approved';
+  
+  -- Check if there are any delayed borrowings
+  IF (delayed_count > 0) THEN
+    -- Raise an error and prevent the new borrowing from being inserted
+    SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'User has delayed borrowings. New borrowing not allowed.';
+  END IF;
+END //
