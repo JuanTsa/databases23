59fcdb6fd55a300f2c31247f36f633c862391730
Ioannis (Juan) Tsantilas <134544768+JuanTsa@users.noreply.github.com>
2023-06-03 15:28:28 +0300

    Create new_reserv.c

---
+++ New_Dummy_Data/new_reserv.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main(){
+        int user=10;
+	int id=1;
+
+	//10 copies of book_id = 14 reserved for user_id = 10-19
+	for(int i=0; i<10; i++) printf("(%i, 14, %i, '2023-06-02', 'Approved'),\n", id++, user++);
+   
+	user=10;
+	//10 copies of book_id = 29 reserved for user_id = 10-19
+	for(int i=0; i<10; i++) printf("(%i, 29, %i, '2023-06-02', 'Approved'),\n", id++, user++);
+
+	user=20;
+	//5 copies of book_id = 32 reserved for user_id = 20-24
+	for(int i=0; i<5; i++) printf("(%i, 32, %i, '2023-06-02', 'Approved'),\n", id++, user++);
+
+	user=20;
+	//5 copies of book_id = 35 reserved for user_id = 20-24
+	for(int i=0; i<5; i++) printf("(%i, 35, %i, '2023-06-02', 'Approved'),\n", id++, user++);
+
+	user=111;
+	//5 copies of book_id = 32 reserved for user_id = 111-115
+	for(int i=0; i<5; i++) printf("(%i, 32, %i, '2023-06-02', 'Approved'),\n", id++, user++);
+
+	user=111;
+	//5 copies of book_id = 35 reserved for user_id = 111-115
+	for(int i=0; i<5; i++) printf("(%i, 35, %i, '2023-06-02', 'Approved'),\n", id++, user++);
+}	
