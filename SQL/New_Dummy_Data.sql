052a72287bf7b3300ec699b78d2dcf311186cfe8
Ioannis (Juan) Tsantilas <134544768+JuanTsa@users.noreply.github.com>
2023-06-03 03:49:03 +0300

    Create New_Dummy_Data.sql

---
+++ SQL/New_Dummy_Data.sql
@@ -0,0 +1,7 @@
+INSERT INTO `School_Unit` (`School_Name`, `Address`, `Phone`, `Email`, `Principal_Name`, `Principal_Surname`)
+VALUES
+('School A', 'Address A', 123456789, 'schoola1@school.com', 'John', 'Smith'),
+('School B', 'Address B', 987654321, 'schoolb2@school.com', 'Emily', 'Johnson'),
+('School C', 'Address C', 456789123, 'schoolc3@school.com', 'Daniel', 'Brown'),
+('School D', 'Address D', 321654987, 'schoold4@school.com', 'Michael', 'Davis'),
+('School E', 'Address E', 789123456, 'schoole5@school.com', 'Emma', 'Miller');
