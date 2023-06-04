bee167588381b8257403ad419aad379afc174d61
Ioannis (Juan) Tsantilas <134544768+JuanTsa@users.noreply.github.com>
2023-06-04 03:14:00 +0300

    Update 4_Triggers.sql

--- SQL/4_Triggers.sql
+++ SQL/4_Triggers.sql
@@ -451,13 +451,3 @@ BEGIN
 END //
 
 DELIMETER ;
-
--- ----------
--- EVENT 1
--- ----------
--- Check old reservations and delete them
-CREATE EVENT delete_old_reservations
-ON SCHEDULE EVERY 1 HOUR
-DO
-  DELETE FROM Reservation
-  WHERE Request_Date < DATE_SUB(CURRENT_DATE(), INTERVAL 7 DAY);
